085a9e6fdc9398a0f17b739ec19128a62dd68756
KenKout <54569460+KenKout@users.noreply.github.com>
2025-11-01 20:23:17 +0000

    chore: initialize Next.js project with TypeScript and Tailwind CSS

---
+++ frontend/next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  'allowedDevOrigins': ['*'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
